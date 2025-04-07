b62dcc7bad4fea73d76850634026be8b1ca4acea
Alejandro <agaviria1408@gmail.com>
2025-04-07 18:02:03 -0500

    se elabora requisitos que llevara la función

---
+++ fabrica.py
@@ -0,0 +1,11 @@
+#una fabrica de productos ceramicos
+#elabora 10 productos para CORONA
+#cada producto esta identificado por
+#id,referencia,(8 caracteres),descripcion,
+#precio de fabricacion individual
+#numero de piezas fabricadas
+#numero de piezas defectuosas
+#fecha de envio de las piezas
+
+#construya una funcion para agregar piezas al pedido
+#construya una funcion para ver el pedido
