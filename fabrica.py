4f7949cd2e12a715318686c1a3351b9846706d0d
Alejandro <agaviria1408@gmail.com>
2025-04-07 18:44:21 -0500

    Se crean las funciones agregarPieza y crearPieza

--- fabrica.py
+++ fabrica.py
@@ -9,3 +9,19 @@
 
 #construya una funcion para agregar piezas al pedido
 #construya una funcion para ver el pedido
+
+def agregarPieza(lista,pieza):
+    lista.append(pieza)
+    return lista
+
+def crearPieza(id, referencia, descripcion, precio, numeroFabricadas, numeroDefectuosas, fechaEnvio):
+    pieza = {
+        "id": id,
+        "referencia": referencia,
+        "descripcion": descripcion,
+        "precio": precio,
+        "numeroFabricadas": numeroFabricadas,
+        "numeroDefectuosas": numeroDefectuosas,
+        "fechaEnvio": fechaEnvio
+    }
+    return pieza
